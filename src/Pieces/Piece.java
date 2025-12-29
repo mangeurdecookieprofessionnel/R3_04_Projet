0b1e611eb2a9efcdba4e9d0885709995ec1e202c
Maxime-Crdvlt <courdavaultmaxime@gmail.com>
2025-12-30 00:27:38 +0100

    Ajout d'une classe abstraite pour les pièces (factorisation)

---
+++ src/Pieces/Piece.java
@@ -0,0 +1,32 @@
+package Pieces;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import Appli.IPiece;
+import Appli.Square;
+import Modele.Couleur;
+
+public abstract class Piece implements IPiece {
+	protected Couleur couleur;
+    protected char lettre;
+    
+    public Couleur getCouleur() {
+        return couleur;
+    }
+    
+    public char getLettre() {
+        return lettre;
+    }
+    
+    public Set<Square> aplatir(List<List<Square>> trajectoires) {
+    	Set<Square> list = new HashSet<>();
+    	for (List<Square> chemin : trajectoires) {
+            list.addAll(chemin);
+        }
+		return list;
+    }
+    
+    public abstract List<List<Square>> mouvement(Square square);
+}
